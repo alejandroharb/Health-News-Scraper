82ad9fdebc3e27323418b84d4f0f0ad5bea3eb6a
alejandro harb <alejandro harb>
2017-03-14 13:39:59 -0500

    finished comment section data retrieval and updating comment feed

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -14,17 +14,17 @@ $(document).ready(function () {
                 var id = elem._id;
                 var commentArr = elem.comment;
                 createArticleCard(title, articleLink, imgLink, id, commentArr);
-                
+
             })
-                
+
         })
     });
-   $('<input type="submit">').on('click', function(e) {
-       e.preventDefault();
-        $.post('/newComment',function(response) {
-            console.log(response)
-        })
-   })
+
+    $('<input type="submit">').on('click', function (e) {
+        e.preventDefault();
+        console.log("inside ajax post function!!")
+        
+    })
 })
 
 function createArticleCard(title, link, imgLink, id, comments) {
@@ -47,32 +47,28 @@ function createArticleCard(title, link, imgLink, id, comments) {
     var contentDiv = $('<div>').attr('class', 'card-content').append(contentTitle);
     //---------Comment reveal Section-----------
     var commentsTitle = $('<span>').attr('class', 'card-title grey-text text-darken-4').html(' Comments <i class="material-icons right">close</i>');
-    var commentsDiv = $('<div>').attr('id', 'commentsInsert'); 
+    var commentsDiv = $('<div>').attr('id', 'commentsInsert');
     //form
     var commentText = $('<input>').attr({
+        class: id,
         type: "text",
         name: "comment",
         placeholder: "Enter Comment Here!"
     })
-    var idInput = $('<input>').attr({
-        type: "hidden",
-        value: id,
-        name: "articleId"
-    })
-    var formSubmitBtn = $('<input type="submit" value="Submit" class="btn">')
+
+    var formSubmitBtn = $('<input value="Submit" type="submit" class="btn">')
     var commentFormInput = $('<form>').attr({
-        class: 'row', 
-        action: "/newComment", 
-        method: "POST"
-    }).append(commentText).append(idInput).append(formSubmitBtn);
-    comments.forEach(function(elem, index) {
-        var newP = $('<p>').html('<i class="material-icons">chat_bubble_outline</i>   '  +  elem.body);
+        class: 'row',
+        action: 'javascript:handleCommentPost("'+id+'")'
+    }).append(commentText).append(formSubmitBtn);
+    comments.forEach(function (elem, index) {
+        var newP = $('<p>').html('<i class="material-icons">chat_bubble_outline</i>   ' + elem.body);
         commentsDiv.append(newP);
     })
     var cardRevealDiv = $('<div>').attr('class', 'card-reveal').append(commentsTitle).append(commentsDiv).append(commentFormInput)
     cardDiv.append(imgDiv).append(contentDiv).append(cardRevealDiv);
     columnDiv.append(cardDiv);
 
-    
+
     $('#articles_wrapper').append(columnDiv);
 }
